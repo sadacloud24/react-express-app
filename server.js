1f4a69a405d67c00c52f145ec0324a3c9608caa4
sadacloud24 <167418216+sadacloud24@users.noreply.github.com>
2025-08-07 14:10:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// API route
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+// Catch-all to serve React for any other route
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
